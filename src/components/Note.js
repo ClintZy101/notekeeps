8eccea3b7dc01797a5837fd37ee646737260f61f
ClintZy101 <clielizek.joy@gmail.com>
2022-02-19 05:17:26 +0900

    grid screen for notes fixed

--- src/components/Note.js
+++ src/components/Note.js
@@ -6,9 +6,10 @@ import { BsBell } from 'react-icons/bs'
 import { BsFillPinFill, BsFillCheckCircleFill } from 'react-icons/bs'
 import { BsCloudCheckFill } from 'react-icons/bs'
 
-export const Note = ({noteModalIsOpen, handleNoteModal}) => {
+export const Note = ({noteModalIsOpen, handleNoteModal, title, content, id}) => {
+    
     return (
-        <div className="border shadow-sm w-full md:w-64 mx-auto gap-2   rounded-md grid  relative group hover:shadow-lg px-3 "
+        <div className="border shadow-sm w-full max-h-max md:w-64 mx-auto gap-2   rounded-md grid  relative group hover:shadow-lg px-3 "
         onClick={handleNoteModal}
         >
             <span>
@@ -17,8 +18,8 @@ export const Note = ({noteModalIsOpen, handleNoteModal}) => {
             <span>
                 <BsFillPinFill className="text-gray-500 absolute right-2  opacity-0 group-hover:opacity-100 transition delay-300" />
             </span>
-        <h1 className="text-center font-medium">Title</h1>
-        <p className="font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, illum ipsa unde commodi ducimus voluptate consequuntur totam consequatur mollitia est minus doloremque suscipit fugit cupiditate repellat quam sapiente voluptatem. Enim!</p>
+        <h1 className="text-center font-medium">{title}</h1>
+        <p className="font-light">{content}</p>
         
         <div className={`flex space-x-5 text-gray-500 my-2 mx-auto opacity-0 group-hover:opacity-100 transition delay-300`}>
             <BsCardImage className="cursor-pointer hover:text-black" />
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,6 +2,26 @@ import React from 'react'
 import { Note } from './Note'
 
 export const Notes = ({noteModalIsOpen, handleNoteModal}) => {
+    const notes = [
+        {
+            id: 1,
+            title: 'title',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, illum ipsa unde commodi ducimus voluptate consequuntur totam consequatur mollitia est minus doloremque suscipit fugit cupiditate repellat quam sapiente voluptatem. Enim!'
+        },
+       
+        {
+            id: 2,
+            title: 'title num 2',
+            content: 'some text paragraph'
+        },
+       
+        {
+            id: 3,
+            title: 'Title Num 3',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, illum ipsa unde commodi ducimus voluptate consequuntur totam consequatur mollitia est minus doloremque suscipit fugit cupiditate repellat quam sapiente voluptatem. Enim!Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, illum ipsa unde commodi ducimus voluptate consequuntur totam consequatur mollitia est minus doloremque suscipit fugit cupiditate repellat quam sapiente voluptatem. Enim!'
+        },
+       
+    ]
     return (
         <div className=" w-full  ">
 
@@ -10,13 +30,18 @@ export const Notes = ({noteModalIsOpen, handleNoteModal}) => {
                 <div className="">
                     <h1>Pinned</h1>
                 </div>
-                <div className="gap-3 p-3 flex flex-wrap transition delay-300 content-start ">
-                    <Note handleNoteModal={handleNoteModal} noteModalIsOpen={noteModalIsOpen} />
-                    <Note />
-                    <Note />
-                    <Note />
-                    <Note />
-                    <Note />
+                <div className="gap-3 p-3 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 transition delay-300  ">
+                    {notes.map((note, index) => (
+                        <Note 
+                        handleNoteModal={handleNoteModal} noteModalIsOpen={noteModalIsOpen} 
+                        key={note.id}
+                        id={note.id}
+                        title={note.title}
+                        content={note.content}
+                        />
+                    ))}
+                    
+
                 </div>
             
             </div>
